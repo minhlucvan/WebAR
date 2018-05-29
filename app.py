be172674a2a90d33d034144193a6813e1b8f9bc1
minhlucvan <luk.mink@gmail.com>
2018-05-29 07:53:01 +0700

    refactor face_regc

--- app.py
+++ app.py
@@ -2,32 +2,43 @@
 from flask import Flask, render_template, send_from_directory
 from flask_socketio import SocketIO, emit
 import base64
-from face_stream import face_stream
+from face_regc import face_stream
+import cv2
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+
+face_regc = face_stream.FaceStream(
+            input_dir = './face_regc/input_dir',
+            modelfile = './face_regc/pre_model/20180402-114759/20180402-114759.pb',
+            mtcnn_dir = './face_regc/d_npy',
+            classifier_filename = './face_regc/face_stream/my_class/my_classifier.pkl'
+        )
+
 
 @app.route('/public/<path:path>')
 def index(path):
     return send_from_directory('public', path)
 
 @socketio.on('frame')
-def test_message(message):
+def on_frame(message):
     print("received")
 
-    fd = open("./face_stream/tmp/input.jpg", "wb")
-    decoded = binary_data = base64.b64decode(message['data'])
+    fd = open("./face_regc/tmp/input.jpg", "wb")
+    decoded = base64.b64decode(message['data'])
     fd.write(decoded)
     fd.close()
 
-    with open("./face_stream/tmp/output.jpg", "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read()).decode('ascii')
-        emit('frame', {'data': encoded_string })
 
-@socketio.on('my broadcast event')
-def test_message(message):
-    emit('my response', {'data': message['data']}, broadcast=True)
+    image = face_regc.detect_faces("./face_regc/tmp/input.jpg")
+    _, buffer = cv2.imencode('.jpg', image)
+    encoded_string = base64.b64encode(buffer)
+    emit('frame', {'data': encoded_string })
+
+    # with open("./face_stream/tmp/output.jpg", "rb") as image_file:
+    #     encoded_string = base64.b64encode(image_file.read()).decode('ascii')
+    #     emit('frame', {'data': encoded_string })
 
 @socketio.on('connect')
 def test_connect():
@@ -38,8 +49,5 @@ def test_disconnect():
     print('Client disconnected')
 
 if __name__ == '__main__':
-    t = face_stream.FaceStream(1, print)
-    t.start()
     socketio.run(app)
-    t.cancel()
 	
