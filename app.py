7f208f9e76e56fd9cd6e59881b24db5a3d1f7420
minhlucvan <minh.lucvan@nccsoft.vn>
2018-06-03 17:16:02 +0700

    adding image download config

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ eventlet.monkey_patch()
 
 import os
 from flask import Flask, render_template
-from flask import make_response
 from flask_socketio import SocketIO, emit
 import cv2
 import base64
@@ -13,8 +12,9 @@ from face_regc import face_stream
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-async_mode = None
-socketio = SocketIO(app)
+async_mode = 'eventlet'
+socketio = SocketIO(app, async_mode=async_mode)
+training = False
 
 
 face_regc = face_stream.FaceStream(
@@ -24,32 +24,62 @@ face_regc = face_stream.FaceStream(
             classifier_filename = './face_regc/my_class/my_classifier.pkl'
         )
 
+
+
+import string
+import random
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
 @app.route('/')
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
 
-@socketio.on('frame')
+@socketio.on('image_frame')
 def frame_message(message):
 
-    print("received")
+    if training == True:
+        return
+        
+    print("image_frame")
     fd = open("./face_regc/tmp/input.jpg", "wb")
     decoded = base64.b64decode(message['data'])
     fd.write(decoded)
     fd.close()
 
 
-    image = face_regc.detect_faces("./face_regc/tmp/input.jpg")
+    image = face_regc.detect_faces("./face_regc/tmp/input.jpg", "./face_regc/tmp/output.jpg")
     _, buffer = cv2.imencode('.jpg', image)
-    encoded_string = base64.b64encode(buffer)
-    emit('frame', {'data': encoded_string })
+    encoded_string = base64.b64encode(buffer).decode('ascii')
+    emit('image_frame', {'data': encoded_string })
 
-    # with open("./face_stream/tmp/output.jpg", "rb") as image_file:
-    #     encoded_string = base64.b64encode(image_file.read()).decode('ascii')
-    #     emit('frame', {'data': encoded_string })
 
-@socketio.on('message')
-def handle_message(message):
-    print('received message: ' + message)
+@socketio.on('add_input')
+def add_input(message):
+    print("add_input")
+
+    file = id_generator(12)
+    directory = "./face_regc/input_dir/" + message['name']
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    fd = open(directory + "/" + file+ ".jpg", "wb")
+    decoded = base64.b64decode(message['data'])
+    fd.write(decoded)
+    fd.close()
+
+@socketio.on('re_train')
+def retrain():
+    training = True
+    os.system("cd ./face_regc && python3 ./aligndata_first.py && python3 ./create_classifier_se.py")
+    face_regc.load_model()
+    training = False
+
+
+@socketio.on_error()
+def chat_error_handler(e):
+    print('An error has occurred: ' + str(e))
+
 
 @socketio.on('connect')
 def test_connect():
@@ -61,4 +91,4 @@ def test_disconnect():
     print('Client disconnected')
 
 if __name__ == '__main__':                             
-    socketio.run(app, debug=True, certfile='cert.pem', keyfile='key.pem', port=5500)
+    socketio.run(app, debug=True, port=5100)
